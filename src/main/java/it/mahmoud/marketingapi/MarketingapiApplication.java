e650e5764b4f5fea355583034e964831b961ea46
m.elawamy <m.elawamy@reply.it>
2025-02-17 11:55:53 +0100

    Initial commit

---
+++ src/main/java/it/mahmoud/marketingapi/MarketingapiApplication.java
@@ -0,0 +1,13 @@
+package it.mahmoud.marketingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketingapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/mahmoud/marketingapi/MarketingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.mahmoud.marketingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
